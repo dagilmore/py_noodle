1d83f0d0651312a9dc600cd81df1f9aaa603cbb2
David Gilmore <dgilmore@3441990.local>
2013-06-25 21:29:07 -0500

    Begin front end development

--- py_noodle/blog/forms.py
+++
@@ -1,9 +0,0 @@
-__author__ = 'dagilmore'
-
-from flask.ext.wtf import Form, TextField, BooleanField
-from flask.ext.wtf import Required
-
-
-class LoginForm(Form):
-    openid = TextField('openid', validators = [Required()])
-    remember_me = BooleanField('remember_me', default = False)
--- runserver.py
+++ runserver.py
@@ -1,10 +1,15 @@
 __author__ = 'dagilmore'
 
 from flask import Flask
-from py_noodle.blog.views import blog
+from flask.ext.assets import Environment
 from database import init_db
+from py_noodle.blog.controller import blog
+
 
 app = Flask(__name__)
+
+#Create assets
+assets = Environment(app)
 
 #register base view controller
 app.register_blueprint(blog, url_prefix='')
@@ -14,4 +19,4 @@ app.config.from_object('config')
 
 if __name__ == '__main__':
     init_db()
-    app.run()
+    app.run(debug=True)
